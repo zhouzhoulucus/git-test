56ee62a790076eff0e65297d95f324974554f839
zhouLucus <zhouLucus@itcast.cn>
2019-10-30 15:52:56 +0800

    把本地工程添加到git

---
+++ git-test-project/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,17 @@
+package cn.itcast.git;
+
+/**
+ * @author zhouLucus
+ * @create 2019-10-30 15:43
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
